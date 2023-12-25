303c55587cb72a8e5d9344ebf5259c6791e93721
jadavnishith <mrnick076@gmail.com>
2023-12-25 11:54:40 +0530

    user input

---
+++ index.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main()
+{
+    //iput data from user
+    
+    int a;
+    printf("Enter Your Number ");
+    scanf("%d",&a);
+    printf("%d\n",a);
+    
+    float f;
+    printf("Enter Your float Number ");
+    scanf("%f",&f);
+    printf("%0.2f\n",f);
+    
+    char d;
+    printf("Enter Your charcter ");
+    scanf("%c",&d);
+    printf("%c\n",d);
+    
+    return 0;
+}
